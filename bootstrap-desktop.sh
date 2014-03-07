d23c2a4be8448414d49d66bb8c3afa2d50601bff
Damon Oehlman <damon.oehlman@gmail.com>
2014-03-07 17:43:12 +1100

    install sourcecodepro font

--- bootstrap-desktop.sh
+++ bootstrap-desktop.sh
@@ -12,20 +12,37 @@
 # 
 # To be completed
 
+# a few defines
+VERSION_SOURCECODEPRO=1.017
+
 # install required dependencies to get up and running
 sudo apt-get install \
-
-    # git
+    curl unzip \
     git \
-
-    # virtual box video drivers and guest additions
     virtualbox-ose-guest-utils virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms
     
 # clone dotfiles
-git clone https://github.com/DamonOehlman/dotfiles.git ~/dotfiles
+[[ -d ~/dotfiles ]] || git clone https://github.com/DamonOehlman/dotfiles.git ~/dotfiles
 
 # initialise helpful links
 rm -rf ~/.config
 ln -s ~/dotfiles/config ~/.config
-ln -s ~/dotfiles/.bashrc-custom ~/.bashrc-custom
-echo ". ~/.bashrc-custom" >> ~/.bashrc
+ln -fs ~/dotfiles/.bashrc-custom ~/.bashrc-custom
+
+if [[ ! $(cat ~/.bashrc | grep bashrc-custom) ]];
+then
+  printf "\n. ~/.bashrc-custom\n" >> ~/.bashrc
+fi
+
+# ensure we have a Downloads folder
+mkdir -p ~/Downloads
+
+# install the adobe source code pro font
+if [[ ! -d ~/.fonts/SourceCodePro ]]; then
+  rm -rf ~/Downloads/SourceCodePro*
+  wget http://downloads.sourceforge.net/project/sourcecodepro.adobe/SourceCodePro_FontsOnly-${VERSION_SOURCECODEPRO}.zip -O ~/Downloads/SourceCodePro.zip
+  unzip ~/Downloads/SourceCodePro.zip -d ~/Downloads
+  mkdir -p ~/.fonts
+  mv ~/Downloads/SourceCodePro_* ~/.fonts/SourceCodePro
+  fc-cache -vf
+fi
